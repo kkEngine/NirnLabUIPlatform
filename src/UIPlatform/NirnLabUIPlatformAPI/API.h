5de4e934685861801c3cbf25974341d6ecfa3cdf
kkEngine <78795688+kkEngine@users.noreply.github.com>
2025-09-24 14:02:59 +0300

    - Simplified API files

--- src/UIPlatform/NirnLabUIPlatformAPI/API.h
+++ src/UIPlatform/NirnLabUIPlatformAPI/API.h
@@ -119,10 +119,44 @@ namespace NL::UI
         IUIPlatformAPI* API = nullptr;
     };
 
-    ResponseVersionMessage GetUIPlatformAPIVersion();
-    bool CreateOrGetUIPlatformAPI(IUIPlatformAPI** a_outApi, NL::UI::Settings* a_settings);
-    bool CreateOrGetUIPlatformAPIWithVersionCheck(NL::UI::IUIPlatformAPI** a_outApi,
-                                                  NL::UI::Settings* a_settings,
-                                                  std::uint32_t a_requestLibVersion,
-                                                  const char* a_requestLibName);
+    inline HMODULE GetNirnLabUILib()
+    {
+        const auto handle = LoadLibrary(NL::UI::LibVersion::PROJECT_NAME);
+        if (handle == nullptr)
+        {
+            const auto errCode = GetLastError();
+            switch (errCode)
+            {
+            case 126:
+                throw std::runtime_error(std::format("{}.dll not found", NL::UI::LibVersion::PROJECT_NAME));
+                break;
+            default:
+                throw std::runtime_error(std::format("Error while loading {}.dll, code: {}", NL::UI::LibVersion::PROJECT_NAME, errCode));
+                break;
+            }
+        }
+
+        return handle;
+    }
+
+    inline ResponseVersionMessage GetUIPlatformAPIVersion()
+    {
+        const auto funcPtr = reinterpret_cast<decltype(&GetUIPlatformAPIVersion)>(GetProcAddress(GetNirnLabUILib(), NameOf(GetUIPlatformAPIVersion)));
+        return funcPtr();
+    }
+
+    inline bool CreateOrGetUIPlatformAPI(IUIPlatformAPI** a_outApi, NL::UI::Settings* a_settings)
+    {
+        const auto funcPtr = reinterpret_cast<decltype(&CreateOrGetUIPlatformAPI)>(GetProcAddress(GetNirnLabUILib(), NameOf(CreateOrGetUIPlatformAPI)));
+        return funcPtr(a_outApi, a_settings);
+    }
+
+    inline bool CreateOrGetUIPlatformAPIWithVersionCheck(NL::UI::IUIPlatformAPI** a_outApi,
+                                                         NL::UI::Settings* a_settings,
+                                                         std::uint32_t a_requestLibVersion,
+                                                         const char* a_requestLibName)
+    {
+        const auto funcPtr = reinterpret_cast<decltype(&CreateOrGetUIPlatformAPIWithVersionCheck)>(GetProcAddress(GetNirnLabUILib(), NameOf(CreateOrGetUIPlatformAPIWithVersionCheck)));
+        return funcPtr(a_outApi, a_settings, a_requestLibVersion, a_requestLibName);
+    }
 }
