7c486f882927968eef53a08518ad0563f7f4899f
kkEngine <78795688+kkEngine@users.noreply.github.com>
2025-11-09 16:51:14 +0300

    - Split key input logic into a separate class

---
+++ src/UIPlatform/Converters/KeyInputConverter.cpp
@@ -0,0 +1,139 @@
+#include "KeyInputConverter.h"
+
+namespace NL::Converters
+{
+    std::uint32_t KeyInputConverter::GetVirtualKey(const std::uint32_t a_scanCode)
+    {
+        std::uint32_t vkCode = 0;
+        RE::BSInputDeviceManager::GetSingleton()->GetDeviceMappedKeycode(RE::INPUT_DEVICES::kKeyboard, a_scanCode, vkCode);
+        return vkCode;
+    }
+
+    void KeyInputConverter::NextKeyboardLayout()
+    {
+        s_currentHKL = ActivateKeyboardLayout((HKL)HKL_NEXT, 0);
+    }
+
+    wchar_t KeyInputConverter::VkCodeToChar(const std::uint32_t a_scanCode, const std::uint32_t a_vkCode, const bool a_shift)
+    {
+        // auto keyboard = RE::BSInputDeviceManager::GetSingleton()->GetKeyboard();
+        static uint8_t s_state[256] = {0};
+        s_state[VK_SHIFT] = a_shift ? 0xFF : 0;
+
+        wchar_t unicodeChar;
+        if (ToUnicodeEx(a_vkCode, a_scanCode, s_state, &unicodeChar, 1, 0, s_currentHKL) != 1)
+        {
+            return L'\0';
+        }
+
+        return unicodeChar;
+    }
+
+    void KeyInputConverter::Clear()
+    {
+        m_currentModifiers = 0;
+    }
+
+    void KeyInputConverter::UpdateCefKeyModifiers(const cef_event_flags_t a_flags, bool a_isKeyDown)
+    {
+        if (a_isKeyDown)
+        {
+            m_currentModifiers |= a_flags;
+        }
+        else
+        {
+            m_currentModifiers &= ~a_flags;
+        }
+    }
+
+    void KeyInputConverter::UpdateModifiersFromVK(const std::uint32_t a_vkCode, bool a_isKeyDown)
+    {
+        if (a_vkCode >= VK_NUMPAD0 && a_vkCode <= VK_DIVIDE)
+        {
+            UpdateCefKeyModifiers(EVENTFLAG_IS_KEY_PAD, a_isKeyDown);
+        }
+        else
+        {
+            switch (a_vkCode)
+            {
+            case VK_CAPITAL:
+                UpdateCefKeyModifiers(EVENTFLAG_CAPS_LOCK_ON, a_isKeyDown);
+                break;
+            case VK_SHIFT:
+                UpdateCefKeyModifiers(EVENTFLAG_SHIFT_DOWN, a_isKeyDown);
+                break;
+            case VK_CONTROL:
+                UpdateCefKeyModifiers(EVENTFLAG_CONTROL_DOWN, a_isKeyDown);
+                break;
+            case VK_MENU:
+                UpdateCefKeyModifiers(EVENTFLAG_ALT_DOWN, a_isKeyDown);
+                break;
+            case VK_NUMLOCK:
+                UpdateCefKeyModifiers(EVENTFLAG_NUM_LOCK_ON, a_isKeyDown);
+                break;
+            case VK_LCONTROL:
+                UpdateCefKeyModifiers(EVENTFLAG_CONTROL_DOWN, a_isKeyDown);
+                UpdateCefKeyModifiers(EVENTFLAG_IS_LEFT, a_isKeyDown);
+                break;
+            case VK_LMENU:
+                UpdateCefKeyModifiers(EVENTFLAG_ALT_DOWN, a_isKeyDown);
+                UpdateCefKeyModifiers(EVENTFLAG_IS_LEFT, a_isKeyDown);
+                break;
+            case VK_LSHIFT:
+                UpdateCefKeyModifiers(EVENTFLAG_SHIFT_DOWN, a_isKeyDown);
+                UpdateCefKeyModifiers(EVENTFLAG_IS_LEFT, a_isKeyDown);
+                break;
+            case VK_RCONTROL:
+                UpdateCefKeyModifiers(EVENTFLAG_CONTROL_DOWN, a_isKeyDown);
+                UpdateCefKeyModifiers(EVENTFLAG_IS_RIGHT, a_isKeyDown);
+                break;
+            case VK_RMENU:
+                UpdateCefKeyModifiers(EVENTFLAG_ALT_DOWN, a_isKeyDown);
+                UpdateCefKeyModifiers(EVENTFLAG_IS_RIGHT, a_isKeyDown);
+                break;
+            case VK_RSHIFT:
+                UpdateCefKeyModifiers(EVENTFLAG_SHIFT_DOWN, a_isKeyDown);
+                UpdateCefKeyModifiers(EVENTFLAG_IS_RIGHT, a_isKeyDown);
+                break;
+            default:
+                break;
+            }
+        }
+    }
+
+    void KeyInputConverter::ProcessButton(const RE::ButtonEvent* a_event)
+    {
+        const auto isKeyStateChanged = a_event->IsDown() || a_event->IsUp();
+        if (isKeyStateChanged)
+        {
+            const auto scanCode = a_event->GetIDCode();
+            const auto vkCode = GetVirtualKey(scanCode);
+            UpdateModifiersFromVK(vkCode, a_event->IsDown());
+
+            CefKeyEvent keyEvent{};
+            keyEvent.size = sizeof(keyEvent);
+            keyEvent.windows_key_code = vkCode;
+            // keyEvent.native_key_code = scanCode;
+            keyEvent.modifiers = m_currentModifiers;
+
+            if (a_event->IsDown())
+            {
+                keyEvent.type = KEYEVENT_RAWKEYDOWN;
+                OnKeyDown(keyEvent);
+
+                const auto wchar = VkCodeToChar(scanCode, vkCode, m_currentModifiers & (EVENTFLAG_SHIFT_DOWN | EVENTFLAG_CAPS_LOCK_ON));
+                if (wchar != '\0')
+                {
+                    keyEvent.type = KEYEVENT_CHAR;
+                    keyEvent.windows_key_code = wchar;
+                    OnChar(keyEvent);
+                }
+            }
+            else
+            {
+                keyEvent.type = KEYEVENT_KEYUP;
+                OnKeyUp(keyEvent);
+            }
+        }
+    }
+}
---
+++ src/UIPlatform/Converters/KeyInputConverter.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "PCH.h"
+
+namespace NL::Converters
+{
+    class KeyInputConverter
+    {
+    protected:
+        static inline HKL s_currentHKL = GetKeyboardLayout(0);
+
+        std::uint32_t m_currentModifiers = 0;
+
+    public:
+        static std::uint32_t GetVirtualKey(const std::uint32_t a_scanCode);
+        static void NextKeyboardLayout();
+        static wchar_t VkCodeToChar(const std::uint32_t a_scanCode, const std::uint32_t a_vkCode, const bool a_shift);
+
+        sigslot::signal_st<CefKeyEvent&> OnKeyDown;
+        sigslot::signal_st<CefKeyEvent&> OnKeyUp;
+        sigslot::signal_st<CefKeyEvent&> OnChar;
+
+        void Clear();
+        void UpdateCefKeyModifiers(const cef_event_flags_t a_flags, bool a_isKeyDown);
+        void UpdateModifiersFromVK(const std::uint32_t a_vkCode, bool a_isKeyDown);
+        void ProcessButton(const RE::ButtonEvent* a_event);
+    };
+}
